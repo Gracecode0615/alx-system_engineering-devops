cfb831933ba71295584225fd6d6c55b12c44591f
Grace0615 <graceebruwiyor@gmail.com>
2024-06-24 12:50:48 +0100

    update

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,23 +1,6 @@
-# This Puppet manifest ensures the wp-settings.php file exists, contains default content if it doesn't, and has correct permissions to fix the Apache 500 error.
-
-file { '/var/www/html/wp-settings.php':
-  ensure  => file,
-  content => "This is a placeholder for wp-settings.php.\n", # Replace with actual default content as necessary
-  owner   => 'www-data',
-  group   => 'www-data',
-  mode    => '0644',
-}
-
+i# Fixes a faulty wordpress site
 exec { 'fix-wordpress':
-  command => 'sed -i \'s/phpp/php/g\' /var/www/html/wp-settings.php',
-  path    => ['/usr/local/bin', '/bin', '/usr/bin'],
-  onlyif  => 'grep -q phpp /var/www/html/wp-settings.php',
-  require => File['/var/www/html/wp-settings.php'],
+  command => 'bash -c "sed -i s/class-wp-locale.phpp/class-wp-locale.php/ \
+/var/www/html/wp-settings.php; service apache2 restart"',
+  path    => '/usr/bin:/usr/sbin:/bin'
 }
-
-service { 'apache2':
-  ensure    => running,
-  enable    => true,
-  subscribe => File['/var/www/html/wp-settings.php'],
-}
-
